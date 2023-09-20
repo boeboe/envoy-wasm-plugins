520b3656b2567497bc7ed3b5d4088e6cb553ed68
Bart Van Bos <bart@tetrate.io>
2023-09-21 01:02:14 +0200

    macos does not support oneline in Make (old version of make), moved to bash script, get rid of buggy metallb helm crap

---
+++ make.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+
+K8S_VERSION=1.27.3
+CLUSTER_NAME=istio-wasm
+ISTIO_VERSION=1.18.2
+
+KUBECTL="kubectl --context kind-${CLUSTER_NAME}"
+HELM="helm --kube-context kind-${CLUSTER_NAME}"
+
+if [[ $1 = "kind-up" ]]; then
+
+	if ! $(kind get clusters | grep -q ${CLUSTER_NAME}) ; then 
+		kind create cluster --name ${CLUSTER_NAME} --image kindest/node:v${K8S_VERSION} ; 
+	else 
+		echo "Kind cluster ${CLUSTER_NAME} already exists" ; 
+	fi
+	k8s_api_ip=$(${KUBECTL} -n kube-system get pod -l component=kube-apiserver -o=jsonpath="{.items[0].metadata.annotations.kubeadm\.kubernetes\.io/kube-apiserver\.advertise-address\.endpoint}")
+	export metallb_startip=$(echo ${k8s_api_ip} | awk -F '.' "{ print \$1\".\"\$2\".\"\$3\".100\";}")
+	export metallb_stopip=$(echo ${k8s_api_ip} | awk -F '.' "{ print \$1\".\"\$2\".\"\$3\".200\";}")
+	if ! $(${HELM} ls -n metallb-system | grep -q metallb) ; then 
+		${HELM} install metallb metallb/metallb -n metallb-system --create-namespace
+	else
+		${HELM} upgrade metallb metallb/metallb -n metallb-system
+	fi
+	${KUBECTL} --context kind-${CLUSTER_NAME} apply -f - << EOF
+$(envsubst < kubernetes/metallb-poolconfig.yaml)
+EOF
+
+  exit 0
+fi
+
+
+if [[ $1 = "istio-up" ]]; then
+	if ! $(${HELM} ls -n istio-system | grep -q istio-base) ; then 
+		${HELM} install istio-base tetratelabs/base -n istio-system --create-namespace --version ${ISTIO_VERSION}
+	else
+		${HELM} upgrade istio-base tetratelabs/base -n istio-system --version ${ISTIO_VERSION}
+	fi
+	if ! $(${HELM} ls -n istio-system | grep -q istiod) ; then 
+		${HELM} install istiod tetratelabs/istiod -n istio-system --version ${ISTIO_VERSION}
+	else
+		${HELM} upgrade istiod tetratelabs/istiod -n istio-system --version ${ISTIO_VERSION}
+	fi
+	if ! $(${HELM} ls -n istio-ingress | grep -q istio-ingress) ; then 
+		${HELM} install istio-ingress tetratelabs/istio-ingress -n istio-ingress --create-namespace --version ${ISTIO_VERSION}
+	else
+		${HELM} upgrade istio-ingress tetratelabs/istio-ingress -n istio-ingress --version ${ISTIO_VERSION}
+	fi
+
+  exit 0
+fi
+
+
+if [[ $1 = "kind-down" ]]; then
+  kind delete cluster --name ${CLUSTER_NAME}
+  exit 0
+fi
+
+echo "please specify action ./make.sh kind-up/istio-up/kind-down"
+exit 1
