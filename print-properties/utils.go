3cd895228c7e643831e5411e2c8f715233a23d08
Bart Van Bos <bartvanbos@gmail.com>
2023-09-23 14:16:34 +0200

    refactor to have a good set of getPropertyXYZ based on commenly used []byte encodings of float64, unint64, map[string]string and []string types

--- print-properties/utils.go
+++ print-properties/utils.go
@@ -3,55 +3,147 @@ package main
 import (
 	"encoding/binary"
 	"math"
-	"strconv"
+	"unsafe"
 
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 )
 
-// convert array of tuplets to map
-func tupletArrayToMap(inArray [][2]string) map[string]string {
-	outMap := make(map[string]string)
-	for _, tuple := range inArray {
-		outMap[tuple[0]] = tuple[1]
+// get string property
+func getPropertyString(path []string) (string, error) {
+	b, err := proxywasm.GetProperty(path)
+	if err != nil {
+		return "", err
 	}
-	return outMap
+
+	return string(b), nil
 }
 
-// safely get string value from map
-func getStringValueFromMap(inMap map[string]string, key string) string {
-	if outString, ok := inMap[key]; ok {
-		return outString
+// get uint64 property
+func getPropertyUint64(path []string) (uint64, error) {
+	b, err := proxywasm.GetProperty(path)
+	if err != nil {
+		return 0, err
 	}
-	proxywasm.LogDebugf("map '%v' does not contain key '%v'\n", inMap, key)
-	return ""
+
+	return deserializeToFUint64(b), nil
 }
 
-// safely get int value from map
-func getIntValueFromMap(inMap map[string]string, key string) int {
-	if outString, ok := inMap[key]; ok {
-		return int(float64fromByteArray([]byte(outString)))
+// get float64 property
+func getPropertyFloat64(path []string) (float64, error) {
+	b, err := proxywasm.GetProperty(path)
+	if err != nil {
+		return 0, err
 	}
-	proxywasm.LogDebugf("map '%v' does not contain key '%v'\n", inMap, key)
-	return 0
+
+	return deserializeToFloat64(b), nil
 }
 
-// safely get bool value from map
-func getBoolValueFromMap(inMap map[string]string, key string) bool {
-	if outString, ok := inMap[key]; ok {
-		outBool, err := strconv.ParseBool(outString)
-		if err != nil {
-			proxywasm.LogErrorf("error converting string '%v' to bool: %v", outString, err)
-			return false
-		}
-		return outBool
+// // get complex property object as a map of byte slices
+// // to be used when dealing with mixed type properties
+// func getPropertyByteSliceMap(path []string) (map[string][]byte, error) {
+// 	b, err := proxywasm.GetProperty(path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return deserializeToByteMap(b), nil
+// }
+
+// get complex property object as a map of string
+// to be used when dealing with string only type properties
+func getPropertyStringMap(path []string) (map[string]string, error) {
+	b, err := proxywasm.GetProperty(path)
+	if err != nil {
+		return nil, err
 	}
-	proxywasm.LogDebugf("map '%v' does not contain key '%v'\n", inMap, key)
-	return false
+
+	return deserializeToStringMap(b), nil
 }
 
-// convert byte array to float64
-func float64fromByteArray(bytes []byte) float64 {
+// get complex property object as a map of string
+// to be used when dealing with string only type properties
+func getPropertyStringSlice(path []string) ([]string, error) {
+	b, err := proxywasm.GetProperty(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return deserializeToStringSlice(b), nil
+}
+
+// deserialize byte slice to string slice
+func deserializeToStringSlice(bs []byte) []string {
+	numStrings := int(binary.LittleEndian.Uint32(bs[:4]))
+	ret := make([]string, numStrings)
+	idx := 4
+	dataIdx := 4 + 8*numStrings
+	for i := 0; i < numStrings; i++ {
+		strLen := int(binary.LittleEndian.Uint64(bs[idx : idx+8]))
+		idx += 8
+		ret[i] = string(bs[dataIdx : dataIdx+strLen])
+		dataIdx += strLen + 2
+	}
+	return ret
+}
+
+// deserialize byte array to float64
+func deserializeToFloat64(bytes []byte) float64 {
 	bits := binary.LittleEndian.Uint64(bytes)
 	float := math.Float64frombits(bits)
 	return float
+}
+
+// deserialize byte array to float64
+func deserializeToFUint64(bytes []byte) uint64 {
+	return binary.LittleEndian.Uint64(bytes)
+}
+
+// // deserialize byte slice to key value map, used for mixed type maps
+// //   - keys are always string
+// //   - value are raw byte strings that need further parsing
+// func deserializeToByteMap(bs []byte) map[string][]byte {
+// 	numHeaders := binary.LittleEndian.Uint32(bs[0:4])
+// 	var sizeIndex = 4
+// 	var dataIndex = 4 + 4*2*int(numHeaders)
+// 	ret := make(map[string][]byte)
+// 	for i := 0; i < int(numHeaders); i++ {
+// 		keySize := int(binary.LittleEndian.Uint32(bs[sizeIndex : sizeIndex+4]))
+// 		sizeIndex += 4
+// 		keyPtr := bs[dataIndex : dataIndex+keySize]
+// 		key := *(*string)(unsafe.Pointer(&keyPtr))
+// 		dataIndex += keySize + 1
+
+// 		valueSize := int(binary.LittleEndian.Uint32(bs[sizeIndex : sizeIndex+4]))
+// 		sizeIndex += 4
+// 		valuePtr := bs[dataIndex : dataIndex+valueSize]
+// 		value := *(*[]byte)(unsafe.Pointer(&valuePtr))
+// 		dataIndex += valueSize + 1
+// 		ret[key] = value
+// 	}
+// 	return ret
+// }
+
+// deserialize byte array to key value map, used for string only type maps
+//   - keys are always string
+//   - value are always string
+func deserializeToStringMap(bs []byte) map[string]string {
+	numHeaders := binary.LittleEndian.Uint32(bs[0:4])
+	var sizeIndex = 4
+	var dataIndex = 4 + 4*2*int(numHeaders)
+	ret := make(map[string]string)
+	for i := 0; i < int(numHeaders); i++ {
+		keySize := int(binary.LittleEndian.Uint32(bs[sizeIndex : sizeIndex+4]))
+		sizeIndex += 4
+		keyPtr := bs[dataIndex : dataIndex+keySize]
+		key := *(*string)(unsafe.Pointer(&keyPtr))
+		dataIndex += keySize + 1
+
+		valueSize := int(binary.LittleEndian.Uint32(bs[sizeIndex : sizeIndex+4]))
+		sizeIndex += 4
+		valuePtr := bs[dataIndex : dataIndex+valueSize]
+		value := *(*string)(unsafe.Pointer(&valuePtr))
+		dataIndex += valueSize + 1
+		ret[key] = value
+	}
+	return ret
 }
--- print-properties/wasm_properties.go
+++ print-properties/wasm_properties.go
@@ -3,66 +3,64 @@
 package main
 
 import (
-	"encoding/binary"
-
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 )
 
 // Get plugin name
 // This matches <metadata.name>.<metadata.namespace> in the istio WasmPlugin CR
 func getPluginName() string {
-	pluginName, err := proxywasm.GetProperty([]string{"plugin_name"})
+	pluginName, err := getPropertyString([]string{"plugin_name"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute plugin_name: %v", err)
 		return ""
 	}
-	return string(pluginName)
+	return pluginName
 }
 
 // Get plugin root id
 // This matches the <spec.pluginName> in the istio WasmPlugin CR
 func getPluginRootId() string {
-	pluginRootId, err := proxywasm.GetProperty([]string{"plugin_root_id"})
+	pluginRootId, err := getPropertyString([]string{"plugin_root_id"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute plugin_root_id: %v", err)
 		return ""
 	}
-	return string(pluginRootId)
+	return pluginRootId
 }
 
 // Get plugin vm id
 //
 // TODO: this seems to be always empty?
 func getPluginVmId() string {
-	pluginVmId, err := proxywasm.GetProperty([]string{"plugin_vm_id"})
+	pluginVmId, err := getPropertyString([]string{"plugin_vm_id"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute plugin_vm_id: %v", err)
 		return ""
 	}
-	return string(pluginVmId)
+	return pluginVmId
 }
 
 // Get upstream cluster name
 //
 // Example value: "outbound|80||httpbin.org"
 func getClusterName() string {
-	clusterName, err := proxywasm.GetProperty([]string{"cluster_name"})
+	clusterName, err := getPropertyString([]string{"cluster_name"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute cluster_name: %v", err)
 		return ""
 	}
-	return string(clusterName)
+	return clusterName
 }
 
 // Get route name (only available in the response path)
 // This matches the <spec.http.name> in the istio VirtualService CR
 func getRouteName() string {
-	routeName, err := proxywasm.GetProperty([]string{"route_name"})
+	routeName, err := getPropertyString([]string{"route_name"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute route_name: %v", err)
 		return ""
 	}
-	return string(routeName)
+	return routeName
 }
 
 type trafficDirection int
@@ -93,13 +91,13 @@ func (t trafficDirection) String() string {
 //   - INBOUND: 1
 //   - OUTBOUND: 2
 func getListenerDirection() trafficDirection {
-	listenerDirection, err := proxywasm.GetProperty([]string{"listener_direction"})
+	listenerDirection, err := getPropertyUint64([]string{"listener_direction"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute listener_direction: %v", err)
 		return 0
 	}
 
-	return trafficDirection(binary.LittleEndian.Uint32(listenerDirection))
+	return trafficDirection(int(listenerDirection))
 }
 
 // Get node id, an opaque node identifier for the envoy node. It also provides the local
@@ -107,171 +105,306 @@ func getListenerDirection() trafficDirection {
 //
 // Example value: router~10.244.0.22~istio-ingress-6d78c67d85-qsbtz.istio-ingress~istio-ingress.svc.cluster.local
 func getNodeId() string {
-	nodeId, err := proxywasm.GetProperty([]string{"node", "id"})
+	nodeId, err := getPropertyString([]string{"node", "id"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute node.id: %v", err)
 	}
-	return string(nodeId)
+	return nodeId
 }
 
 // Get node cluster, which defines the local service cluster name where envoy is running
 //
 // Example value: istio-ingress.istio-ingress
 func getNodeCluster() string {
-	nodeCluster, err := proxywasm.GetProperty([]string{"node", "cluster"})
+	nodeCluster, err := getPropertyString([]string{"node", "cluster"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading wasm attribute node.cluster: %v", err)
 	}
-	return string(nodeCluster)
+	return nodeCluster
 }
 
 // struct containing node bootstrap configuration
 type nodeMetadata struct {
-	annotations         string
+	annotations         map[string]string
+	appContainers       string
 	clusterId           string
 	envoyPrometheusPort int
 	envoyStatusPort     int
 	instanceIps         string
+	interceptionMode    string
 	istioProxySha       string
 	istioVersion        string
-	labels              string
+	labels              map[string]string
 	meshId              string
 	name                string
 	namespace           string
 	nodeName            string
 	owner               string
-	pilotSan            string
-	proxyConfig         string
+	pilotSan            []string
+	podPorts            string
+	proxyConfig         proxyConfig
 	serviceAccount      string
-	unprivilegedPod     bool
 	workloadName        string
+}
+
+type proxyConfig struct {
+	binaryPath               string
+	concurrency              int
+	configPath               string
+	controlPlaneAuthPolicy   string
+	discoveryAddress         string
+	drainDuration            string
+	extraStatTags            []string
+	proxyAdminPort           int
+	serviceCluster           string
+	statNameLength           int
+	statusPort               int
+	terminationDrainDuration string
+	tracing                  tracing
+}
+
+type tracing struct {
+	address string
 }
 
 // Get node metadata, extending the node identifier
 func getNodeMetadata() nodeMetadata {
 	result := nodeMetadata{}
+	var err error
 
-	nodeMetadataArray, err := proxywasm.GetPropertyMap([]string{"node", "metadata"})
+	result.annotations, err = getPropertyStringMap([]string{"node", "metadata", "ANNOTATIONS"})
 	if err != nil {
-		proxywasm.LogWarnf("error reading node.metadata: %v", err)
-		return result
+		proxywasm.LogWarnf("error reading node.metadata.ANNOTATIONS: %v", err)
 	}
-	nodeMetadataMap := tupletArrayToMap(nodeMetadataArray)
+	result.appContainers, err = getPropertyString([]string{"node", "metadata", "APP_CONTAINERS"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.APP_CONTAINERS: %v", err)
+	}
+	result.clusterId, err = getPropertyString([]string{"node", "metadata", "CLUSTER_ID"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.CLUSTER_ID: %v", err)
+	}
+	envoyPrometheusPortFloat64, err := getPropertyFloat64([]string{"node", "metadata", "ENVOY_PROMETHEUS_PORT"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.ENVOY_PROMETHEUS_PORT: %v", err)
+	}
+	result.envoyPrometheusPort = int(envoyPrometheusPortFloat64)
+	envoyStatusPortFloat64, err := getPropertyFloat64([]string{"node", "metadata", "ENVOY_STATUS_PORT"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.ENVOY_STATUS_PORT: %v", err)
+	}
+	result.envoyStatusPort = int(envoyStatusPortFloat64)
+	result.instanceIps, err = getPropertyString([]string{"node", "metadata", "INSTANCE_IPS"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.INSTANCE_IPS: %v", err)
+	}
+	result.interceptionMode, err = getPropertyString([]string{"node", "metadata", "INTERCEPTION_MODE"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.INTERCEPTION_MODE: %v", err)
+	}
+	result.istioProxySha, err = getPropertyString([]string{"node", "metadata", "ISTIO_PROXY_SHA"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.ISTIO_PROXY_SHA: %v", err)
+	}
+	result.istioVersion, err = getPropertyString([]string{"node", "metadata", "ISTIO_VERSION"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.ISTIO_VERSION: %v", err)
+	}
+	result.labels, err = getPropertyStringMap([]string{"node", "metadata", "LABELS"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.LABELS: %v", err)
+	}
+	result.meshId, err = getPropertyString([]string{"node", "metadata", "MESH_ID"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.MESH_ID: %v", err)
+	}
+	result.name, err = getPropertyString([]string{"node", "metadata", "NAME"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.NAME: %v", err)
+	}
+	result.namespace, err = getPropertyString([]string{"node", "metadata", "NAMESPACE"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.NAMESPACE: %v", err)
+	}
+	result.nodeName, err = getPropertyString([]string{"node", "metadata", "NODE_NAME"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.NODE_NAME: %v", err)
+	}
+	result.owner, err = getPropertyString([]string{"node", "metadata", "OWNER"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.OWNER: %v", err)
+	}
+	result.pilotSan, err = getPropertyStringSlice([]string{"node", "metadata", "PILOT_SAN"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PILOT_SAN: %v", err)
+	}
+	result.podPorts, err = getPropertyString([]string{"node", "metadata", "POD_PORTS"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.POD_PORTS: %v", err)
+	}
+	result.serviceAccount, err = getPropertyString([]string{"node", "metadata", "SERVICE_ACCOUNT"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.SERVICE_ACCOUNT: %v", err)
+	}
+	result.workloadName, err = getPropertyString([]string{"node", "metadata", "WORKLOAD_NAME"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.WORKLOAD_NAME: %v", err)
+	}
 
-	result.annotations = getStringValueFromMap(nodeMetadataMap, "ANNOTATIONS")
-	result.clusterId = getStringValueFromMap(nodeMetadataMap, "CLUSTER_ID")
-	result.envoyPrometheusPort = getIntValueFromMap(nodeMetadataMap, "ENVOY_PROMETHEUS_PORT")
-	result.envoyStatusPort = getIntValueFromMap(nodeMetadataMap, "ENVOY_STATUS_PORT")
-	result.instanceIps = getStringValueFromMap(nodeMetadataMap, "INSTANCE_IPS")
-	result.istioProxySha = getStringValueFromMap(nodeMetadataMap, "ISTIO_PROXY_SHA")
-	result.istioVersion = getStringValueFromMap(nodeMetadataMap, "ISTIO_VERSION")
-	result.labels = getStringValueFromMap(nodeMetadataMap, "LABELS")
-	result.meshId = getStringValueFromMap(nodeMetadataMap, "MESH_ID")
-	result.name = getStringValueFromMap(nodeMetadataMap, "NAME")
-	result.namespace = getStringValueFromMap(nodeMetadataMap, "NAMESPACE")
-	result.nodeName = getStringValueFromMap(nodeMetadataMap, "NODE_NAME")
-	result.owner = getStringValueFromMap(nodeMetadataMap, "OWNER")
-	result.pilotSan = getStringValueFromMap(nodeMetadataMap, "PILOT_SAN")
-	result.proxyConfig = getStringValueFromMap(nodeMetadataMap, "PROXY_CONFIG")
-	result.serviceAccount = getStringValueFromMap(nodeMetadataMap, "SERVICE_ACCOUNT")
-	result.unprivilegedPod = getBoolValueFromMap(nodeMetadataMap, "UNPRIVILEGED_POD")
-	result.workloadName = getStringValueFromMap(nodeMetadataMap, "WORKLOAD_NAME")
+	result.proxyConfig = proxyConfig{}
+	result.proxyConfig.binaryPath, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "binaryPath"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.binaryPath: %v", err)
+	}
+	proxyConfigConcurrencyFloat64, err := getPropertyFloat64([]string{"node", "metadata", "PROXY_CONFIG", "concurrency"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.concurrency: %v", err)
+	}
+	result.proxyConfig.concurrency = int(proxyConfigConcurrencyFloat64)
+	result.proxyConfig.configPath, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "configPath"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.configPath: %v", err)
+	}
+	result.proxyConfig.controlPlaneAuthPolicy, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "controlPlaneAuthPolicy"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.controlPlaneAuthPolicy: %v", err)
+	}
+	result.proxyConfig.discoveryAddress, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "discoveryAddress"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.discoveryAddress: %v", err)
+	}
+	result.proxyConfig.drainDuration, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "drainDuration"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.drainDuration: %v", err)
+	}
+	result.proxyConfig.extraStatTags, err = getPropertyStringSlice([]string{"node", "metadata", "PROXY_CONFIG", "extraStatTags"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.extraStatTags: %v", err)
+	}
+	proxyConfigProxyAdminPortFloat64, err := getPropertyFloat64([]string{"node", "metadata", "PROXY_CONFIG", "proxyAdminPort"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.proxyAdminPort: %v", err)
+	}
+	result.proxyConfig.proxyAdminPort = int(proxyConfigProxyAdminPortFloat64)
+	result.proxyConfig.serviceCluster, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "serviceCluster"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.serviceCluster: %v", err)
+	}
+	proxyConfigStatNameLengthFloat64, err := getPropertyFloat64([]string{"node", "metadata", "PROXY_CONFIG", "statNameLength"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.statNameLength: %v", err)
+	}
+	result.proxyConfig.statNameLength = int(proxyConfigStatNameLengthFloat64)
+	proxyConfigStatusPortFloat64, err := getPropertyFloat64([]string{"node", "metadata", "PROXY_CONFIG", "statusPort"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.statusPort: %v", err)
+	}
+	result.proxyConfig.statusPort = int(proxyConfigStatusPortFloat64)
+	result.proxyConfig.terminationDrainDuration, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "terminationDrainDuration"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.terminationDrainDuration: %v", err)
+	}
+	result.proxyConfig.tracing.address, err = getPropertyString([]string{"node", "metadata", "PROXY_CONFIG", "tracing", "zipkin", "address"})
+	if err != nil {
+		proxywasm.LogWarnf("error reading node.metadata.PROXY_CONFIG.tracing.zipkin.address: %v", err)
+	}
 
 	return result
 }
 
 func getNodeDynamicParams() string {
-	nodeDynamicParams, err := proxywasm.GetProperty([]string{"node", "dynamic_parameters"})
+	nodeDynamicParams, err := getPropertyString([]string{"node", "dynamic_parameters", "params"})
 	if err != nil {
-		proxywasm.LogWarnf("error reading node.dynamic_parameters: %v", err)
+		proxywasm.LogWarnf("error reading node.dynamic_parameters.params: %v", err)
 	}
-	return string(nodeDynamicParams)
+	return nodeDynamicParams
 }
 
 func getNodeLocality() string {
-	nodeLocality, err := proxywasm.GetProperty([]string{"node", "locality"})
+	nodeLocality, err := getPropertyString([]string{"node", "locality", "region"})
 	if err != nil {
-		proxywasm.LogWarnf("error reading node.locality: %v", err)
+		proxywasm.LogWarnf("error reading node.locality.region: %v", err)
 	}
-	return string(nodeLocality)
+	return nodeLocality
 }
 
 func getNodeUserAgentName() string {
-	nodeUserAgentName, err := proxywasm.GetProperty([]string{"node", "user_agent_name"})
+	nodeUserAgentName, err := getPropertyString([]string{"node", "user_agent_name"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.user_agent_name: %v", err)
 	}
-	return string(nodeUserAgentName)
+	return nodeUserAgentName
 }
 
+// Not used by istio
 func getNodeUserAgentVersion() string {
-	nodeUserAgentVersion, err := proxywasm.GetProperty([]string{"node", "user_agent_version"})
+	nodeUserAgentVersion, err := getPropertyString([]string{"node", "user_agent_version"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.user_agent_version: %v", err)
 	}
-	return string(nodeUserAgentVersion)
+	return nodeUserAgentVersion
 }
 
-func getNodeUserAgentBuildVersion() string {
-	nodeUserAgentBuildVersion, err := proxywasm.GetProperty([]string{"node", "user_agent_build_version"})
+func getNodeUserAgentBuildVersion() map[string]string {
+	nodeUserAgentBuildVersion, err := getPropertyStringMap([]string{"node", "user_agent_build_version", "metadata"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.user_agent_build_version: %v", err)
 	}
-	return string(nodeUserAgentBuildVersion)
+	return nodeUserAgentBuildVersion
 }
 
 func getNodeExtensions() string {
-	nodeExtensions, err := proxywasm.GetProperty([]string{"node", "extensions"})
+	nodeExtensions, err := getPropertyString([]string{"node", "extensions"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.extensions: %v", err)
 	}
-	return string(nodeExtensions)
+	return nodeExtensions
 }
 
 func getNodeClientFeatures() string {
-	nodeClientFeatures, err := proxywasm.GetProperty([]string{"node", "client_features"})
+	nodeClientFeatures, err := getPropertyString([]string{"node", "client_features"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.extensions: %v", err)
 	}
-	return string(nodeClientFeatures)
+	return nodeClientFeatures
 }
 
 func getNodeListeningAddresses() string {
-	nodeListeningAddresses, err := proxywasm.GetProperty([]string{"node", "listening_addresses"})
+	nodeListeningAddresses, err := getPropertyString([]string{"node", "listening_addresses"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.listening_addresses: %v", err)
 	}
-	return string(nodeListeningAddresses)
+	return nodeListeningAddresses
 }
 
 func getClusterMetadata() string {
-	clusterMetadata, err := proxywasm.GetProperty([]string{"node", "cluster_metadata"})
+	clusterMetadata, err := getPropertyString([]string{"node", "cluster_metadata"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.cluster_metadata: %v", err)
 	}
-	return string(clusterMetadata)
+	return clusterMetadata
 }
 
 func getListenerMetadata() string {
-	listenerMetadata, err := proxywasm.GetProperty([]string{"node", "listener_metadata"})
+	listenerMetadata, err := getPropertyString([]string{"node", "listener_metadata"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.listener_metadata: %v", err)
 	}
-	return string(listenerMetadata)
+	return listenerMetadata
 }
 
 func getRouteMetadata() string {
-	routeMetadata, err := proxywasm.GetProperty([]string{"node", "route_metadata"})
+	routeMetadata, err := getPropertyString([]string{"node", "route_metadata"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.route_metadata: %v", err)
 	}
-	return string(routeMetadata)
+	return routeMetadata
 }
 
 func getUpstreamHostMetadata() string {
-	upstreamHostMetadata, err := proxywasm.GetProperty([]string{"node", "upstream_host_metadata"})
+	upstreamHostMetadata, err := getPropertyString([]string{"node", "upstream_host_metadata"})
 	if err != nil {
 		proxywasm.LogWarnf("error reading node.upstream_host_metadata: %v", err)
 	}
-	return string(upstreamHostMetadata)
+	return upstreamHostMetadata
 }
